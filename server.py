b4f04b92fe7efa3026c93222e98b504fa8ac571c
bilal <buddygames.bilal@gmail.com>
2017-12-14 09:52:02 +0100

    Class gemaakt genaamd chat en de fucntie welkom der in egzet

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import MySQLdb
 
-database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="//,db="chat")
+database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="g",db="chat")
 
 cur = database_connectie.cursor()
 
+global welkom_input
 
-def welkom():
-    welkom_input = input("Welkom type 1 om te registreren en 2 om in teloggen ")
-    if welkom_input == "1":
-        registreren()
-    elif welkom_input == "2":
-        inlogen()
+
+class chat:
+    def welkom(self):
+        welkom_input = input("Welkom type 1 om te registreren en 2 om in teloggen ")
+        if welkom_input == "1":
+            registreren()
+        elif welkom_input == "2":
+            inlogen()
