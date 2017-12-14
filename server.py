9b723de2b3f0192b7ff8f35281a5654498009238
bilal <buddygames.bilal@gmail.com>
2017-12-14 09:47:41 +0100

    else if naar elif type fout

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="//hey",db="chat")
+database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="//,db="chat")
 
 cur = database_connectie.cursor()
 
@@ -9,5 +9,5 @@ def welkom():
     welkom_input = input("Welkom type 1 om te registreren en 2 om in teloggen ")
     if welkom_input == "1":
         registreren()
-    else if welkom_input == "2":
+    elif welkom_input == "2":
         inlogen()
