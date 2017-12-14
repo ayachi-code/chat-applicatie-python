4db69d0c0393f2ff8fe0157256b30688749821ed
bilal <buddygames.bilal@gmail.com>
2017-12-14 09:46:27 +0100

    Welkom screen gemaakt

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 import MySQLdb
 
-database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="a",db="chat")
+database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="//hey",db="chat")
 
 cur = database_connectie.cursor()
+
+
+def welkom():
+    welkom_input = input("Welkom type 1 om te registreren en 2 om in teloggen ")
+    if welkom_input == "1":
+        registreren()
+    else if welkom_input == "2":
+        inlogen()
