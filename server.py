e7f71f200962338f3dffc311011333633102942a
bilal <buddygames.bilal@gmail.com>
2017-12-14 10:21:34 +0100

    Mysqldb python3 geinstaleerd en functie login gemaakt

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
 import MySQLdb
 
-database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="g",db="chat")
+database_connectie = MySQLdb.connect(host="localhost",user="root",passwd="test",db="chat")
 
 cur = database_connectie.cursor()
 
-global welkom_input
+
+def inlogen():
+    print("je bent bij het menu inloggen ")
+
+
 
 
 class chat:
     def welkom(self):
+        global welkom_input
         welkom_input = input("Welkom type 1 om te registreren en 2 om in teloggen ")
         if welkom_input == "1":
-            registreren()
-        elif welkom_input == "2":
             inlogen()
-
-
+        elif welkom_input == "2":
+            registreren()
+        return welkom_input
 
 
 
 
 #Definieren
 chat_object = chat()
+
+welkom_scherm = chat_object.welkom()
